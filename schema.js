bd5fbb80c9dae2b634343a88926e876dff42876f
Kevin Han <hankevin0618@gmail.com>
2023-04-11 20:24:49 +1000

    update

--- schema.js
+++ schema.js
@@ -3,7 +3,7 @@ import { loadFilesSync } from '@graphql-tools/load-files'
 import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge'
 import path from 'path';
   const loadedTypes = loadFilesSync(path.join(__dirname, './**/*.typeDefs.js'));
-  const loadedResolvers = loadFilesSync(path.join(__dirname, './**/*.{queries,mutations}.js'))
+  const loadedResolvers = loadFilesSync(path.join(__dirname, './**/*.resolvers.js'))
   const typeDefs = mergeTypeDefs(loadedTypes);
   const resolvers = mergeResolvers(loadedResolvers);
   
